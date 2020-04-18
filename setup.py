4b15e25f4326f63b8453578057e46ba91a94859e
Adam Spierer <anspierer@gmail.com>
2020-04-18 18:03:01 -0400

    clean up

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
-'''Setup for FreeClimber
-'''
-
-from os import path
-from codecs import open
 from setuptools import setup, find_packages
 
-here = path.abspath(path.dirname(__file__))
-
 # Get the long description from the relevant file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open('README.md','r') as f:
     long_description = f.read()
 
 setup(name='FreeClimber',
-      version='0.2',
+      version='0.3',
       description='A background-adjusted, particle detector used to quantify climbing performance for groups of Drosophila in a negative geotaxis assay',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -32,7 +25,7 @@ setup(name='FreeClimber',
 
       packages=find_packages(),
 #       packages=['FreeClimber'],
-      install_requires=['ffmpeg==1.4',"argparse==1.1"
+      install_requires=['ffmpeg==1.4',"argparse==1.1",
                         'pandas','numpy','scipy','pip','matplotlib==3.1.3',
                         'wxPython==4.0.4','trackpy==0.4.2'],
       zip_safe=False)
