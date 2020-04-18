00a907853d83d3266715b1bffb67fda96832daf2
Adam Spierer <anspierer@gmail.com>
2020-04-18 12:41:29 -0400

    Attempting to get conda to build a wheel

--- setup.py
+++ setup.py
@@ -10,18 +10,18 @@ from setuptools import setup, find_packages
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='free_climber',
-      version='0.5',
+setup(name='FreeClimber',
+      version='0.1',
       description='A background-adjusted, particle detector used to quantify climbing performance for groups of Drosophila in a negative geotaxis assay',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      url='http://github.com/adamspierer/free_climber',
+      url='http://github.com/adamspierer/FreeClimber',
       
       author='Adam N. Spierer',
-      author_email='anspierer+free_climber@gmail.com',
+      author_email='anspierer+Github_setup_py@gmail.com',
       license='MIT',
 
       classifiers=[
@@ -30,10 +30,10 @@ setup(name='free_climber',
       'License :: OSI Approved :: MIT License',
       'Programming Language :: Python :: 3.7'],
 
-      keywords='Drosophila climbing particle detection local linear regression',
+      keywords='Drosophila climbing negative geotaxis background subtraction particle detection local linear regression',
 
       packages=setuptools.find_packages(),
-      packages=['free_climber'],
-      install_requires=['os','sys','argparse','subprocess','time','pandas','numpy','scipy',
+      packages=['FreeClimber'],
+      install_requires=['os','sys','argparse','ffmpeg==0.2.0','subprocess','time','pandas','numpy','scipy',
                         'pip','matplotlib==3.1.3','wxPython==4.0.4','trackpy==0.4.2'],
       zip_safe=False)
