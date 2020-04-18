42a29129e75db2a82012bb5296d6611ad08032cc
Adam Spierer <anspierer@gmail.com>
2020-04-18 15:44:42 -0400

    Testing setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='FreeClimber',
       long_description_content_type="text/markdown",
       url='http://github.com/adamspierer/FreeClimber',
       
-      author='Adam N. Spierer',
+      author='Adam Spierer',
       author_email='anspierer+Github_setup_py@gmail.com',
       license='MIT',
 
@@ -32,9 +32,9 @@ setup(name='FreeClimber',
 
       keywords='Drosophila climbing negative geotaxis background subtraction particle detection local linear regression',
 
-      packages=setuptools.find_packages(),
-      packages=['FreeClimber'],
-      install_requires=['os','sys','argparse','ffmpeg==0.2.0','subprocess','time',
+      packages=find_packages(),
+#       packages=['FreeClimber'],
+      install_requires=['ffmpeg==1.4',"argparse==1.1"
                         'pandas','numpy','scipy','pip','matplotlib==3.1.3',
                         'wxPython==4.0.4','trackpy==0.4.2'],
       zip_safe=False)
