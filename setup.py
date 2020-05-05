723fde4116c4826090c526aba74c0d61a2358555
Adam Spierer <anspierer@gmail.com>
2020-05-04 21:28:44 -0400

    Updating microversion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as f:
     long_description = f.read()
 
 setup(name='FreeClimber',
-      version='0.3',
+      version='0.3.1',
       description='FreeClimber is a Python-based background subtraction, particle detector used to estimate the velocity for a group of particles moving from the bottom to the top of an image, using a local linear regression.',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -21,7 +21,7 @@ setup(name='FreeClimber',
       'License :: OSI Approved :: MIT License',
       'Programming Language :: Python :: 3.7'],
 
-      keywords='Drosophila climbing negative geotaxis background subtraction particle detection local linear regression',
+      keywords='Drosophila melanogaster climbing negative geotaxis assay background subtraction particle detection local linear regression high-throughput high throughput automated behavior quantification velocity',
 
       packages=find_packages(),
 #       packages=['FreeClimber'],
