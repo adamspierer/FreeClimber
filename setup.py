09129b46e699b0f01165c30314804a54cea7a196
Adam Spierer <anspierer@gmail.com>
2020-05-20 21:57:00 -0400

    Fixed ffmpeg-python module name and version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as f:
     long_description = f.read()
 
 setup(name='FreeClimber',
-      version='0.3.1',
+      version='0.3.2',
       description='FreeClimber is a Python-based background subtraction, particle detector used to estimate the velocity for a group of particles moving from the bottom to the top of an image, using a local linear regression.',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -26,7 +26,7 @@ setup(name='FreeClimber',
 
       packages=find_packages(),
 #       packages=['FreeClimber'],
-      install_requires=['ffmpeg==1.4',"argparse==1.1",
+      install_requires=['ffmpeg-python==0.2.0',"argparse==1.1",
                         'pandas','numpy','scipy','pip','matplotlib==3.1.3',
                         'wxPython==4.0.4','trackpy==0.4.2'],
       zip_safe=False)
