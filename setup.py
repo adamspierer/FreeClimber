8b9185715f7b5b10b09f3e86a5dcdce8a33684a9
Adam Spierer <anspierer@gmail.com>
2020-12-21 13:00:33 -0500

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as f:
     long_description = f.read()
 
 setup(name='FreeClimber',
-      version='0.3.2',
+      version='0.4.0',
       description='FreeClimber is a Python-based background subtraction, particle detector used to estimate the velocity for a group of particles moving from the bottom to the top of an image, using a local linear regression.',
       long_description=long_description,
       long_description_content_type="text/markdown",
